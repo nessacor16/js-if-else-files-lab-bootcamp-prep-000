e8c9e29506a69f944e10dc5303cc1e4d36a95933
Vanessa Corlette <nessacor16@gmail.com>
2019-08-07 23:28:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 var name = "Joe"
 var height = "74"
+if (name === "Susan") {
+    console.log("The name is correct")
+  } else {
+      console.log("Susan")
+  }
+    
+    }
+  
 
 
 // Don't worry about this
