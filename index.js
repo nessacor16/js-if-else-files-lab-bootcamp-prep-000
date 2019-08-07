4ef4f4629fc7e34eb0cb49b0602c933cdce2587c
Vanessa Corlette <nessacor16@gmail.com>
2019-08-07 23:48:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,7 +5,12 @@ if (name == "Joe") {
 } else {
   console.log("Joe")
 }
-
+if (height == "74") {
+    console.log("The height is correct")
+} else {
+  console.log("74")
+}
+}
 // Don't worry about this
 module.exports = { name, height
 }
